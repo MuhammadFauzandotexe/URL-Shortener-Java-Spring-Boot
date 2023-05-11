bda5e7050aac41e234c40bb1ca488776c65b49b5
MuhammadFauzandotexe <mfauzan51087@gmail.com>
2023-05-11 23:04:19 +0700

    fix problem service verifikasin

--- src/main/java/com/example/demo/controller/RedirectRoute.java
+++ src/main/java/com/example/demo/controller/RedirectRoute.java
@@ -1,18 +1,17 @@
 package com.example.demo.controller;
 
-import com.example.demo.service.RedirectService;
+import com.example.demo.service.UrlService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-
 @Controller
 @AllArgsConstructor
-public class MyController {
-    private RedirectService redirectService;
+public class RedirectRoute {
+    private UrlService urlService;
     @GetMapping("/{url}")
     public String redirectToPage(@PathVariable("url") String url, Model modell) {
-        return redirectService.getUrl(url);
+        return urlService.getUrl(url);
     }
 }
--- src/main/java/com/example/demo/service/UrlService.java
+++ src/main/java/com/example/demo/service/UrlService.java
@@ -6,12 +6,16 @@ import com.example.demo.model.Url;
 import com.example.demo.repository.AuthorRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import javax.persistence.EntityManager;
 import javax.persistence.EntityNotFoundException;
+import javax.persistence.Query;
 import java.time.LocalDateTime;
 @AllArgsConstructor
 @Service
 public class UrlService {
     private final AuthorRepository authorRepository;
+    private EntityManager entityManager;
     public Author addNewURL(Long id, UrlDTO dto){
         Author author = authorRepository.findById(id).orElseThrow(EntityNotFoundException::new);
         Url url = new Url();
@@ -21,5 +25,13 @@ public class UrlService {
         url.setUpdatedAt(LocalDateTime.now());
         author.addChild(url);
         return authorRepository.save(author);
+    }
+    public String getUrl(String newUrl){
+        String queryString = "SELECT u FROM Url u WHERE newURL = :newUrl";
+        Query query = entityManager.createQuery(queryString,Url.class);
+        query.setParameter("newUrl",newUrl);
+        Url url = (Url) query.getSingleResult();
+        String nativeUrl = url.getNativeURL();
+        return nativeUrl;
     }
 }
